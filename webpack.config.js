3be3a571f3561a5df4e2bb63fa3245c6ba2ba187
v-jiangsen <18698625759@JRA4W42B1140.JDJR.360buyAD.local>
2019-12-13 18:01:01 +0800

    修改

--- webpack.config.js
+++ webpack.config.js
@@ -2,61 +2,78 @@ const path = require("path")
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require("clean-webpack-plugin")
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-module.exports = {
-  entry: {
-    index: "./scripts/index.js",//入口文件，不配置的话默认为src目录下的index.js,
-    // index2: "./scripts/index2.js"
-  },
-  output: {
-    filename: "[name].[hash].bundle.js",//【name】便是入口文件名
-    path: path.join(__dirname, "dist")
-  },
-  module: {//处理对象文件模块
-    rules: [
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']//处理css
-      },
-      {
-        test: /\.(png|jpg|gif)$/,
-        use: [
-          {
-            loader: "url-loader",
-            options: {
-              outputPath: "images/",
-              limit: 500
-            }
-          }
-        ]
-      }
-    ]
-  },
-  optimization: {
-    runtimeChunk: "single",
-    splitChunks: {
-      chunks: 'all',
-      cacheGroups: {
-        commons: {
-          test: /[\\/]scripts[\\/]/,
-          minChunks: 1,//最小引用2次
-          minSize: 0 // 只要超出0字节就生成一个新包
+const OptimizeCss = require("optimize-css-assets-webpack-plugin")
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = env => {
+  console.log(env.NODE_ENV);
+  return {
+    entry: {
+      index: "./scripts/index.js",//入口文件，不配置的话默认为src目录下的index.js,
+      // index2: "./scripts/index2.js"
+    },
+    output: {
+      filename: "[name].[hash].bundle.js",//【name】便是入口文件名
+      path: path.join(__dirname, "/dist/test")
+    },
+    module: {//处理对象文件模块
+      rules: [
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader']//处理css
         },
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
+        {
+          test: /\.(png|jpg|gif)$/,
+          use: [
+            {
+              loader: "url-loader",
+              options: {
+                outputPath: "images/",
+                limit: 500
+              }
+            }
+          ]
+        }
+      ]
+    },
+    optimization: {
+      minimize: env.NODE_ENV == "production",
+      minimizer: [
+        new TerserPlugin({
+          /* your config */
+          test: /\.js(\?.*)?$/i,
+          exclude: /\/node_modules/,
+        }),
+        new OptimizeCss({})
+      ],
+      runtimeChunk: "single",
+      splitChunks: {
+        chunks: 'all',
+        cacheGroups: {
+          commons: {
+            test: /[\\/]scripts[\\/]/,
+            minChunks: 1,//最小引用2次
+            minSize: 0 // 只要超出0字节就生成一个新包
+          },
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+          }
         }
       }
-    }
-  },
-  plugins: [//对应插件
-    new HtmlWebpackPlugin({//配置
-      title: "测试",
-      filename: "index.html",//输出文件名
-      template: "./index.html"//以当前目录下的Index.html文件为模板生成dist/index.html文件
-    }),
-    new CleanWebpackPlugin(),//传入数组，删除指定目录
-    new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css"
-    })
-  ]
+    },
+    plugins: [//对应插件
+      new HtmlWebpackPlugin({//配置
+        title: "测试",
+        filename: "index.html",//输出文件名
+        template: "./index.html"//以当前目录下的Index.html文件为模板生成dist/index.html文件
+      }),
+      new CleanWebpackPlugin(),//传入数组，删除指定目录
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+        chunkFilename: "[id].css"
+      }),
+      // 热更新，热更新不是刷新
+      // new webpack.HotModuleReplacementPlugin()
+    ]
+  }
 }
